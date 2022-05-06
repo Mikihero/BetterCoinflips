479f1dac2dcc4d805f0872567b40d76f9d384f3a
Miki_hero <100715076+Mikihero@users.noreply.github.com>
2022-05-06 13:53:15 +0200

    Add files via upload

---
+++ BetterCoinflips/BetterCoinflips.cs
@@ -0,0 +1,51 @@
+﻿using Exiled.API.Features;
+using System;
+using Exiled.API.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.Events.EventArgs;
+
+using Player = Exiled.Events.Handlers.Player;
+
+
+namespace BetterCoinflips
+{
+    public class BetterCoinflips : Plugin<Config>
+    {
+        private static readonly Lazy<BetterCoinflips> LazyInstance = new Lazy<BetterCoinflips>(valueFactory: () => new BetterCoinflips());
+        public static BetterCoinflips Instance => LazyInstance.Value;
+
+        public override PluginPriority Priority { get; } = PluginPriority.Medium;
+
+        private Handlers.Player player;
+
+        private BetterCoinflips()
+        {
+
+        }
+        public override void OnEnabled()
+        {
+            RegisterEvents();
+        }
+
+        public override void OnDisabled()
+        {
+            UnRegisterEvents();
+        }
+
+        public void RegisterEvents()
+        {
+            player = new Handlers.Player();
+
+            Player.FlippingCoin += player.OnFlippingCoing;
+        }
+        public void UnRegisterEvents()
+        {
+            Player.FlippingCoin -= player.OnFlippingCoing;
+
+            player = null;
+        }
+    }
+}
